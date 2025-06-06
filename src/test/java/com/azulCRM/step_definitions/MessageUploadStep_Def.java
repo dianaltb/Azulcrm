97b908dd3d591a15a5ebcf25e28f25f9a63a9f36
sashk <sashkovnyc@gmail.com>
2025-06-06 14:43:46 -0400

    Framework is updated, redundant pages are deleted

--- src/test/java/com/azulCRM/step_definitions/MessageUploadStep_Def.java
+++ src/test/java/com/azulCRM/step_definitions/MessageUploadStep_Def.java
@@ -1,20 +1,20 @@
 package com.azulCRM.step_definitions;
 
-import com.azulCRM.pages.MessageUploadPage;
+import com.azulCRM.pages.ActivityStreamPage;
 import io.cucumber.java.en.When;
 
 public class MessageUploadStep_Def {
-    MessageUploadPage messageUploadPage = new MessageUploadPage();
+    private final ActivityStreamPage ACTIVITY = new ActivityStreamPage();
 
 
     @When("user clicks on message button in the top left corner")
     public void user_clicks_on_message_button_in_the_top_left_corner() {
-    messageUploadPage.messageBtn.click();
+        ACTIVITY.messageBtn.click();
 
     }
     @When("user clicks on upload files icon")
     public void user_clicks_on_upload_files_icon() {
-        messageUploadPage.uploadFileButton.click();
+        ACTIVITY.uploadFileButton.click();
 
     }
 

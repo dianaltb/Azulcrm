88b3aff369ce50f83cc90d088b89ad831597ba4f
Elena Zenchenko <olenazenchenko@gmail.com>
2025-06-10 17:35:23 -0400

    updated feature and step def

--- src/test/java/com/azulCRM/step_definitions/MessageUpload_Step_Defs.java
+++ src/test/java/com/azulCRM/step_definitions/MessageUpload_Step_Defs.java
@@ -12,11 +12,6 @@ public class MessageUpload_Step_Defs {
         ACTIVITY = new ActivityStreamPage();
     }
 
-    @When("user clicks on message button in the top left corner")
-    public void user_clicks_on_message_button_in_the_top_left_corner() {
-        ACTIVITY.messageTab.click();
-    }
-
     @When("user clicks on upload files icon")
     public void user_clicks_on_upload_files_icon() {
         ACTIVITY.uploadFileButton.click();

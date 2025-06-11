fa33c9ed556cfce566d72bb6014151649784292c
mmamedova <mahmamedova@gmail.com>
2025-06-11 11:45:02 -0400

    Add Activity Stream top menu & MORE tab scenarios with waits, After master merged, test runs smooth, tag removed

--- src/test/java/com/azulCRM/runners/TestRunner.java
+++ src/test/java/com/azulCRM/runners/TestRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/azulCRM/step_definitions",
-        tags = "@TopNavTabs",
+        tags = "",
         dryRun = false,
         publish = false
 )

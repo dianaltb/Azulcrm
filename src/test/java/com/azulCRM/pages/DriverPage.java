e2980951feaedbdbe0979e2d16598ce47d5fc8c1
alexander <alex.voynash@gmail.com>
2025-06-12 10:56:46 -0400

    improved

--- src/test/java/com/azulCRM/pages/DriverPage.java
+++ src/test/java/com/azulCRM/pages/DriverPage.java
@@ -1,11 +1,15 @@
 package com.azulCRM.pages;
 
+import com.azulCRM.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
 public class DriverPage extends BasePage{
+
+    public DriverPage(){PageFactory.initElements(Driver.getDriver(), this);}
 
 
     @FindBy(xpath = "//div[@id='top_menu_id_docs']//span[@class='main-buttons-item-text-title']")
